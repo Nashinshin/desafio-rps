18c87fd8a39d13435f7f2cb9194a2395cbbe0627
Ignacio Juarez <ignaciojuarez66@gmail.com>
2024-04-30 09:48:42 +0200

    1b596e4

---
+++ index.377278e2.js
@@ -0,0 +1,23 @@
+// let counter = 0;
+// const intervalId = setInterval(() =>{
+//     if(counter === 200){
+//         clearInterval(intervalId);
+//     }
+//     else{counter++;
+//         document.querySelector(".contador")!.innerHTML = counter.toString();
+//     }
+// },100);
+// const contador = document.querySelector("body");
+// const style = document.createElement("style");
+// style.innerHTML = `
+// .contador{
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     font-size: 40px;
+//     color: var(--first-color);
+// }
+// `
+// contador!.appendChild(style);
+
+//# sourceMappingURL=index.377278e2.js.map
